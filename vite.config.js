bcf73eb17e6531a341a35a28eca0c4cba3e8c9ff
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-02-19 21:30:24 +0600

    add home page

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        muted: "hsl(var(--muted))",
+        background: "hsl(var(--color-background))",
+        foreground: "hsl(var(--color-foreground))",
+        primary: "hsl(var(--color-primary))",
+      },
+    },
+  },
 });
